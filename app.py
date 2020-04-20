ac974e054bbc0d592c47dd5ee1ef3929814e71cd
Tiago Navarro <tiagornandrade@hotmail.com>
2020-04-20 17:57:49 -0300

    Commit Azure

--- app.py
+++ app.py
@@ -20,31 +20,35 @@ def index():
 
 @app.route('/profile')
 def profile():
-#    if 'username' in session:
-#        return 'Você está logado como ' + session['username']
+    user_profile = mongo.db.users.find({'username': session['username']})
+    return render_template('profile.html',users=user_profile)
 
-    return render_template('profile.html')
+@app.route('/user_suggestion')
+def suggestion():
+    user_suggestion = mongo.db.users.find({'username': session['username']})
+    return render_template('home.html',users=user_suggestion)
 
 @app.route('/home')
 def home():
     user = mongo.db.users.find()
     post = mongo.db.posted.find()
-    user_number = mongo.db.users.count()
     return render_template('home.html',users=user,posted=post)
+
+@app.route('/user_number')
+def user_number():
+    user_number = mongo.db.users.find()
+    user_number_count = user_number.count(True)
+    return render_template('user_number.html',users=user_number_count)
 
 @app.route('/action', methods=['POST'])
 def action():
-#    posted = request.form['post']
-#    mongo.db.posted.insert({'posted' : posted})
-#    return redirect(url_for('home'))
 
     if request.method == 'POST':
         posted = mongo.db.posted
         existing_posted = posted.find_one({'posted' : request.form['post']})
 
         if existing_posted is None:
-            posted.insert({'posted':request.form['post']})
-            #session['username'] =  request.form['username']
+            posted.insert({'username': session['username'], 'posted': request.form['post']})
             return redirect(url_for('home'))
 
         return 'Essa postagem já existe'
@@ -74,7 +78,7 @@ def register():
 
         if existing_user is None:
             #hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'username':request.form['username'], 'password': request.form['password']})
+            users.insert({'name':request.form['nome'], 'username':request.form['username'], 'password': request.form['password'], 'city': request.form['city'], 'state': request.form['state']})
             session['username'] =  request.form['username']
             return redirect(url_for('login'))
 
@@ -99,4 +103,5 @@ def dashboard():
 
 if __name__ == "__main__":
     app.secret_key = "senhasecreta"
-    app.run(threaded=True, port=5000)
+    #app.run(threaded=True, port=5000)
+    app.run(debug=True)
